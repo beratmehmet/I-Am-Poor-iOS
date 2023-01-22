a32dcc1d7077996cadae06957352c756aa01d140
Berat Mehmet Topuz <beratmehemtpz@gmail.com>
2023-01-22 17:58:35 +0300

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Berat_Mehmet_Topuz on 22.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
